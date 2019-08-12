37ccab59d88a5a261699aef54719b3bacaa7d751
mohammad katoozi <k2527806@gmail.com>
2019-08-12 16:47:13 +0430

    update the mux.router variable name

--- cmd/gokit-microservices/main.go
+++ cmd/gokit-microservices/main.go
@@ -10,18 +10,18 @@ import (
 )
 
 func main() {
-	r := mux.NewRouter()
+	router := mux.NewRouter()
 
 	getUser := users.GetUserHandler()
 	updateUser := users.UpdateUserHandler()
 	deleteUser := users.DeleteUserHandler()
 
-	r.Handle("/user/{id}", getUser).Methods("GET")
-	r.Handle("/user/{id}", updateUser).Methods("PUT")
-	r.Handle("/user/{id}", deleteUser).Methods("DELETE")
+	router.Handle("/user/{id}", getUser).Methods("GET")
+	router.Handle("/user/{id}", updateUser).Methods("PUT")
+	router.Handle("/user/{id}", deleteUser).Methods("DELETE")
 
 	srv := &http.Server{
-		Handler:      r,
+		Handler:      router,
 		Addr:         "127.0.0.1:8080",
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
